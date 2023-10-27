0a596ad28a3db36ee40579b4f358c897c782e873
YC.W <contact@wangyuchen.cn>
2023-10-27 11:58:45 +0800

    feat: support KeyPrefix

--- src/easy-logger.spec.ts
+++ src/easy-logger.spec.ts
@@ -55,6 +55,56 @@ describe("EasyLogger", () => {
       expect(processStdoutWriteSpy.firstCall.firstArg).to.include(name);
     });
 
+    it("should support string key prefix", () => {
+      const stringKeyPrefix = "RandomKeyPrefix";
+      const key = "RandomKey";
+      const value = "RandomValue";
+      const name = "TestLogger";
+      const logger = new EasyLogger(name);
+
+      logger.begin()
+      logger.start(stringKeyPrefix)
+      logger.log(key, value);
+      expect(processStdoutWriteSpy.calledOnce).to.be.true;
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(key);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(value);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(name);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(stringKeyPrefix);
+      logger.end()
+    });
+
+    it("should support array key prefix", () => {
+      const arrayKeyPrefix = ["RandomKeyPrefix1", "RandomKeyPrefix2"];
+      const key = "RandomKey";
+      const value = "RandomValue";
+      const name = "TestLogger";
+      const logger = new EasyLogger(name);
+      logger.start(arrayKeyPrefix)
+      logger.log(key, value);
+      expect(processStdoutWriteSpy.calledOnce).to.be.true;
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(key);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(value);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(name);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(arrayKeyPrefix[0]);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(arrayKeyPrefix[1]);
+      logger.end()
+    });
+
+    it("should support clear key prefix", () => {
+      const stringKeyPrefix = "RandomKeyPrefix";
+      const key = "RandomKey";
+      const value = "RandomValue";
+      const name = "TestLogger";
+      const logger = new EasyLogger(name);
+      logger.start(stringKeyPrefix)
+      logger.log(key, value);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.include(stringKeyPrefix)
+      processStdoutWriteSpy.resetHistory()
+      logger.end()
+      logger.log(key, value);
+      expect(processStdoutWriteSpy.firstCall.firstArg).to.not.include(stringKeyPrefix)
+    });
+
     afterEach(() => {
       processStdoutWriteSpy.restore();
     });
--- src/easy-logger.ts
+++ src/easy-logger.ts
@@ -6,18 +6,51 @@ import { LoggerKey } from "./logger-key.type";
 
 export class EasyLogger extends Logger {
   private _contentFormatter: IContentFormatter = new DefaultContentFormatter();
+  private _keyPrefix: string[] = [];
 
   setContentFormatter(contentFormatter: IContentFormatter) {
     this._contentFormatter = contentFormatter;
   }
 
+  /**
+   * when start a method, mark method names
+   * @param keyPrefix
+   */
+  start(keyPrefix: LoggerKey = []) {
+    if (typeof keyPrefix === "string") {
+      this._keyPrefix = [keyPrefix];
+    } else {
+      this._keyPrefix = keyPrefix;
+    }
+  }
+
+  /**
+   * when finish a method, clear side effects
+   */
+  finish() {
+    this._keyPrefix = []; // reset
+  }
+
+  /**
+   * alias of start
+   */
+  begin(keyPrefix: LoggerKey = []) {
+    this._keyPrefix = Array.isArray(keyPrefix) ? keyPrefix : [keyPrefix]
+  }
+
+  /**
+   * alias of finish
+   */
+  end() {
+    this.finish();
+  }
+
   private formatter(key: LoggerKey, value?: any) {
-    if(Array.isArray(key)){
-      return this._contentFormatter.print(key.join(ARRAY_SEPARATOR), value);
-    }
-    else {
-      return this._contentFormatter.print(key, value);
-    }
+    const innerKeys = [
+      ...this._keyPrefix,
+      ...(Array.isArray(key) ? key : [key]),
+    ];
+    return this._contentFormatter.print(innerKeys.join(ARRAY_SEPARATOR), value);
   }
 
   print(key: LoggerKey, value?: any) {
